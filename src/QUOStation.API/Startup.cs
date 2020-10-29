c16ce9b0a8637e3b7f2814a7036e847409adb07e
Flávio Tassan <tassanf@gmail.com>
2020-10-29 01:00:11 -0300

    Startup for WebApi configurantion and dependency injection

--- src/QUOStation.API/Startup.cs
+++ src/QUOStation.API/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using QUOStation.Domain.Interfaces.Services;
+using QUOStation.Services.Rest;
 
 namespace QUOStation.API
 {
@@ -16,6 +18,8 @@ namespace QUOStation.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddTransient<IAwesomeQuotation, AwesomeQuotation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +34,7 @@ namespace QUOStation.API
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
