0495a8b02b6b3752b73c3865cc20be0278077710
Flávio Tassan <tassanf@gmail.com>
2020-10-28 23:55:51 -0300

    Awesome quotation response model

--- src/QUOStation.Domain/Entities/Currency.cs
+++ src/QUOStation.Domain/Entities/Currency.cs
@@ -8,5 +8,14 @@ namespace QUOStation.Domain.Entities
     {
         public string Code { get; set; }
         public string CodeIn { get; set; }
+        public string Name { get; set; }
+        public decimal High { get; set; }
+        public decimal Low { get; set; }
+        public float Variation { get; set; }
+        public double PercentageChange { get; set; }
+        public decimal Bid { get; set; }
+        public decimal Ask { get; set; }
+        public string TimeStamp { get; set; }
+        public string CreateDate { get; set; }
     }
 }
